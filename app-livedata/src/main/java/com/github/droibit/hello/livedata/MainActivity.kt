00a9ecc3c0907bacaafc9980d3416204308d262b
Shinya Kumagai <roomful.rooms@gmail.com>
2018-06-26 15:04:45 +0900

    Update app-livedata with reactive streams

--- app-livedata/src/main/java/com/github/droibit/hello/livedata/MainActivity.kt
+++ app-livedata/src/main/java/com/github/droibit/hello/livedata/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.github.droibit.hello.livedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.toLiveData
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.text
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 class MainActivity : AppCompatActivity() {
+
+  private val liveData: LiveData<String> by lazy {
+    Flowable.just("Hello, from RX")
+        .delay(15_000L, MILLISECONDS)
+        .doOnNext {
+          Log.d(BuildConfig.BUILD_TYPE, "#doOnNext($it)")
+        }
+        .observeOn(Schedulers.io())
+        .doOnSubscribe {
+          Log.d(BuildConfig.BUILD_TYPE, "#doOnSubscribe()")
+        }
+        .toLiveData()
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    liveData.observe(this, Observer {
+      text.text = it!!
+    })
   }
 }
